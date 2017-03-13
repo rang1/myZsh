5ea92c5ee32ccbfc5459c20439d672d74bc06726
ryan <ryan>
2017-03-13 21:32:10 +0100

    added git plugin. fixed zsh.sh

--- .zsh/zsh.sh
+++ .zsh/zsh.sh
@@ -6,12 +6,12 @@ fi
 # Initializes Oh My Zsh
 
 # # add a function path
-# fpath=($ZSH/functions $ZSH/completions $fpath)
+fpath=($ZSH/functions $ZSH/completions $fpath)
 
 # # Load all stock functions (from $fpath files) called below.
-# autoload -U compaudit compinit
+autoload -U compaudit compinit
 
-# : ${ZSH_DISABLE_COMPFIX:=true}
+: ${ZSH_DISABLE_COMPFIX:=true}
 
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
@@ -37,6 +37,31 @@ for plugin ($plugins); do
   fi
 done
 
+# Figure out the SHORT hostname
+if [[ "$OSTYPE" = darwin* ]]; then
+  # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
+  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST=${HOST/.*/}
+else
+  SHORT_HOST=${HOST/.*/}
+fi
+
+# Save the location of the current completion dump file.
+if [ -z "$ZSH_COMPDUMP" ]; then
+  ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+fi
+
+if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
+  # If completion insecurities exist, warn the user without enabling completions.
+  if ! compaudit &>/dev/null; then
+    # This function resides in the "lib/compfix.zsh" script sourced above.
+    handle_completion_insecurities
+  # Else, enable and cache completions to the desired file.
+  else
+    compinit -d "${ZSH_COMPDUMP}"
+  fi
+else
+  compinit -i -d "${ZSH_COMPDUMP}"
+fi
 # Load all of the plugins that were defined in ~/.zshrc
 for plugin ($plugins); do
   if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
